58de3f2bad2237c2620edf5989347b35ee9b3e85
Blue-Pentagram <jamesbennell@gmail.com>
2016-02-01 18:51:55 +1100

    Just a test addon
    This may never be finshed know me but yer.
    it might also be removed later on so yer

---
+++ CrescentRose_Test-WIP/lua/weapons/Crescent Rose/cl_init.lua
@@ -0,0 +1,9 @@
+--Weapon cl_init
+
+include('shared.lua')
+
+SWEP.PrintName = "Crescent Rose"
+SWEP.Slot = 5
+SWEP.SlotPos = 20
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = false
---
+++ CrescentRose_Test-WIP/lua/weapons/Crescent Rose/init.lua
@@ -0,0 +1,10 @@
+--DeathNote Weapon init
+
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+include( 'shared.lua' )
+
+SWEP.Weight = 5
+SWEP.AutoSwitchTo = true
+SWEP.AutoSwitchFrom = true
